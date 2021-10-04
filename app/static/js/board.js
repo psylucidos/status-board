91848a319e6b7a8160e9c764eb1c48d0c6cd6772
psylucidos <psylucidos@gmail.com>
2021-10-04 13:13:22 +1000

    Added simple updating graph and installed jquery

--- app/static/js/board.js
+++ app/static/js/board.js
@@ -1,23 +1,23 @@
 
 let chartElement = document.getElementById('myChart').getContext('2d');
-const labels = ['1', '2', '3', '4', '5', '6', '7'];
+const labels = [];
 const data = {
   labels: labels,
   datasets: [{
     label: 'CPU',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: [],
     fill: false,
     borderColor: 'rgb(75, 192, 120)',
-    tension: 0.1
+    tension: 0.3
   }, {
     label: 'RAM',
-    data: [65, 34, 80, 81, 98, 55, 40],
+    data: [],
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.3
   }, {
     label: 'Requests',
-    data: [67, 21, 55, 77, 98, 40, 32],
+    data: [],
     fill: false,
     borderColor: 'rgb(75, 192, 220)',
     tension: 0.3
@@ -27,4 +27,41 @@ const data = {
 let chart = new Chart(chartElement, {
   type: 'line',
   data: data,
+});
+
+function removeData(chart) {
+  chart.data.labels.shift();
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.shift();
+  });
+  chart.update();
+}
+
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset, i) => {
+    dataset.data.push(data[i]);
+  });
+  chart.update();
+}
+
+function showIncomingChartData(time, cpu, ram, reqs) {
+  addData(chart, time, [cpu, ram, reqs]);
+  if (chart.data.labels.length >= 360) {
+    removeData(chart);
+  }
+}
+
+$(document).ready(function() {
+  setInterval(function () {
+    let date = new Date();
+    let h = date.getHours(),
+        m = date.getMinutes();
+        s = date.getSeconds();
+
+    let cpu = 30,
+        ram = 40,
+        reqs = 10;
+    showIncomingChartData(`${h < 10 ? '0' : ''}${h}:${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`, cpu, ram, reqs);
+  }, 10*1000);
 });
